eb44ef29910e5a2553ad99599f0e11e5edf9236c
Jon Cooke <jonmac@iquit.lan>
2021-07-27 16:48:40 -0600

    Typo fixed, runs now

--- web/posts/urls.py
+++ web/posts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # ex: /posts/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /posts/5/results/
-    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /posts/5/vote/
     path('<int:announcement_id>/vote/', views.vote, name='vote'),
 ]
